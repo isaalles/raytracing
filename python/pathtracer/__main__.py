d4dffd5a205960411986c56fd228720830b517e8
Isabelle Alles <5335758+isaalles@users.noreply.github.com>
2022-12-29 21:30:36 -0800

    use Vec3 class and implement __main__.py for pkg invoking

---
+++ python/pathtracer/__main__.py
@@ -0,0 +1,8 @@
+"""CLI entry point.
+
+To be called with `python -m pathtracer` from inside the dir `python/`.
+"""
+
+from . import render
+
+render.main()
--- python/pathtracer/render.py
+++ python/pathtracer/render.py
@@ -1,28 +1,24 @@
 """Render."""
 
+from .vec3 import Vec3
+
 
 def hello_world(path=None):
     """Hello World render of ppm image file."""
     if not path:
         path = "hello_world.ppm"
 
-    nx = 200
-    ny = 100
+    resx = 200
+    resy = 100
     lines = [
         "P3",
-        f"{nx} {ny}",
+        f"{resx} {resy}",
         "255",
     ]
-    for j in range(ny-1, 0, -1):
-        for i in range(nx):
-            r = i / nx
-            g = j / ny
-            b = 0.2
-            ir = int(255.99 * r)
-            ig = int(255.99 * g)
-            ib = int(255.99 * b)
-
-            lines.append(f"{ir} {ig} {ib}")
+    for j in range(resy-1, 0, -1):
+        for i in range(resx):
+            col = Vec3(i / resx, j / resy, 0.2)
+            lines.append(str((255.99 * col).to_int()))
 
     with open(path, "w", encoding="utf-8") as f:
         f.writelines("\n".join(lines))
@@ -32,7 +28,3 @@ def hello_world(path=None):
 def main():
     """Main entry point."""
     hello_world()
-
-
-if __name__ == "__main__":
-    main()

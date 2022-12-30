3e9510950e8a538eef0bbdbb34fe262efbd4a492
Isabelle Alles <5335758+isaalles@users.noreply.github.com>
2022-12-29 23:37:30 -0800

    draw red sphere

--- python/pathtracer/render.py
+++ python/pathtracer/render.py
@@ -5,35 +5,26 @@ from .vec3 import (unit_vector, Vec3)
 
 
 _WHITE = Vec3(1.0, 1.0, 1.0)
+_RED = Vec3(1.0, 0.0, 0.0)
 _LIGHT_BLUE = Vec3(0.5, 0.7, 1.0)
 
 
-def hello_world(path=None):
-    """Hello World render of ppm image file."""
-    if not path:
-        path = "hello_world.ppm"
-
-    resx = 200
-    resy = 100
-    lines = [
-        "P3",
-        f"{resx} {resy}",
-        "255",
-    ]
-    for j in range(resy-1, -1, -1):
-        for i in range(resx):
-            col = Vec3(i / resx, j / resy, 0.2)
-            lines.append(str((255.99 * col).to_int()))
-
-    with open(path, "w", encoding="utf-8") as f:
-        f.writelines("\n".join(lines))
-        f.write("\n")
+def hit_sphere(center: Vec3, radius: float, ray: Ray) -> bool:
+    """Sphere hit check."""
+    oc = ray.origin - center
+    a = ray.direction.dot(ray.direction)
+    b = 2.0 * oc.dot(ray.direction)
+    c = oc.dot(oc) - radius*radius
+    discriminant = b*b - 4*a*c
+    return discriminant > 0
 
 
-def color_blend(
+def color(
         ray: Ray, start_color: Vec3 = _WHITE, end_color: Vec3 = _LIGHT_BLUE
 ) -> Vec3:
-    """Blend colors vertically."""
+    """Calculate ray color."""
+    if hit_sphere(Vec3(0, 0, -1), 0.5, ray):
+        return _RED
     unit_direction = unit_vector(ray.direction)
     parameter = 0.5 * (unit_direction.y + 1.0)
     return (1 - parameter)*start_color + parameter*end_color  # lerp
@@ -62,7 +53,29 @@ def image(path=None):
             u = i / resx
             v = j / resy
             ray = Ray(origin, lower_left_corner + u*horizontal + v*vertical)
-            col = color_blend(ray)
+            col = color(ray)
+            lines.append(str((255.99 * col).to_int()))
+
+    with open(path, "w", encoding="utf-8") as f:
+        f.writelines("\n".join(lines))
+        f.write("\n")
+
+
+def hello_world(path=None):
+    """Hello World render of ppm image file."""
+    if not path:
+        path = "hello_world.ppm"
+
+    resx = 200
+    resy = 100
+    lines = [
+        "P3",
+        f"{resx} {resy}",
+        "255",
+    ]
+    for j in range(resy-1, -1, -1):
+        for i in range(resx):
+            col = Vec3(i / resx, j / resy, 0.2)
             lines.append(str((255.99 * col).to_int()))
 
     with open(path, "w", encoding="utf-8") as f:

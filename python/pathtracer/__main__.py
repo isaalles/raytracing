effee30a7e8463633cb3845828b59d787104f967
Isabelle Alles <5335758+isaalles@users.noreply.github.com>
2022-12-29 23:09:32 -0800

    cli: add modes so we can run different render functions

--- python/pathtracer/__main__.py
+++ python/pathtracer/__main__.py
@@ -3,6 +3,23 @@
 To be called with `python -m pathtracer` from inside the dir `python/`.
 """
 
+import argparse
+import sys
+
 from . import render
 
-render.main()
+
+def main():
+    """Entry point."""
+    parser = argparse.ArgumentParser(prog="pathtracer")
+    parser.add_argument("mode", choices=("hello-world", "image"))
+    args = parser.parse_args()
+    if args.mode == "hello-world":
+        render.hello_world()
+    else:
+        render.main()
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())

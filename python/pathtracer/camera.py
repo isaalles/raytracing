9f4516e2e5e0e031facef62ee0640cfd1e87616a
Isabelle Alles <5335758+isaalles@users.noreply.github.com>
2022-12-30 19:16:33 -0800

    add camera implementation

---
+++ python/pathtracer/camera.py
@@ -0,0 +1,32 @@
+"""Camera."""
+
+from .ray import Ray
+from .vec3 import Vec3
+
+
+class Camera:
+    """Camera object."""
+
+    def __init__(
+        self,
+        lower_left_corner=Vec3(-2.0, -1.0, -1.0),
+        horizontal=Vec3(4.0, 0.0, 0.0),
+        vertical=Vec3(0.0, 2.0, 0.0),
+        origin=Vec3(0.0, 0.0, 0.0),
+    ):
+        self.lower_left_corner = lower_left_corner
+        self.horizontal = horizontal
+        self.vertical = vertical
+        self.origin = origin
+
+    def get_ray(self, u: float, v: float) -> Ray:
+        """Camera ray."""
+        return Ray(
+            self.origin,
+            (
+                self.lower_left_corner
+                + u * self.horizontal
+                + v * self.vertical
+                - self.origin
+            ),
+        )

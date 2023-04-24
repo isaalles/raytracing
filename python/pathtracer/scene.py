a71dc4cdeeca808c934264dce2534f725260b5b3
Isabelle Alles <5335758+isaalles@users.noreply.github.com>
2023-04-23 20:43:16 -0700

    add random scene with lots of spheres

--- python/pathtracer/scene.py
+++ python/pathtracer/scene.py
@@ -1,11 +1,21 @@
 """Our scene objects."""
+
+import random
+
 from .hittable import HittableList, Sphere
 from . import material
 from .vec3 import Color, Point3
 
 
-def construct_scene(greyshaded=False):
+def construct_scene(greyshaded=False, randomize=False):
     """Construct 3d scene for rendering."""
+    if randomize:
+        return _random_scene()
+    return _manual_scene(greyshaded=greyshaded)
+
+
+def _manual_scene(greyshaded=False):
+    """Manually construct a scene."""
     # Materials
     if greyshaded:
         mat_ground = None
@@ -30,3 +40,43 @@ def construct_scene(greyshaded=False):
     )
 
     return scene
+
+
+def _random_scene():
+    """Construct a random scene."""
+    world = HittableList()
+
+    mat_ground = material.Lambertian(Color(0.5, 0.5, 0.5))
+    world.append(Sphere(Point3(0, -1000, 0), 1000, mat_ground))
+
+    for a in range(-11, 11):
+        for b in range(-11, 11):
+            choose_mat = random.random()
+            center = Point3(a + 0.9 * random.random(), 0.2, b + 0.9 * random.random())
+
+            if (center - Point3(4, 0.2, 0)).length() > 0.9:
+                if choose_mat < 0.8:
+                    # diffuse
+                    albedo = Color.random() * Color.random()
+                    mat_sphere = material.Lambertian(albedo)
+                elif choose_mat < 0.95:
+                    # metal
+                    albedo = Color.random(0.5, 1)
+                    fuzz = random.uniform(0, 0.5)
+                    mat_sphere = material.Metal(albedo, fuzz)
+                else:
+                    # glass
+                    mat_sphere = material.Dielectric(1.5)
+
+                world.append(Sphere(center, 0.2, mat_sphere))
+
+    mat_1 = material.Dielectric(1.5)
+    world.append(Sphere(Point3(0, 1, 0), 1.0, mat_1))
+
+    mat_2 = material.Lambertian(Color(0.4, 0.2, 0.1))
+    world.append(Sphere(Point3(-4, 1, 0), 1.0, mat_2))
+
+    mat_3 = material.Metal(Color(0.7, 0.6, 0.5), 0.0)
+    world.append(Sphere(Point3(4, 1, 0), 1.0, mat_3))
+
+    return world
